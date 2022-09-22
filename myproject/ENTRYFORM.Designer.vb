cd63294f528090bdbcb3c3a1ef76a178413da480
Olaoye Mich <41102062+michson@users.noreply.github.com>
2022-09-22 04:59:28 +0100

    Update ENTRYFORM.Designer.vb

--- myproject/ENTRYFORM.Designer.vb
+++ myproject/ENTRYFORM.Designer.vb
@@ -1,7 +1,6 @@
 <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class ENTRYFORM
     Inherits System.Windows.Forms.Form
-
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
