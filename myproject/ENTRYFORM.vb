15dd421ac577b74491b59b6f43d17efae6d7b4df
Olaoye Mich <41102062+michson@users.noreply.github.com>
2022-09-23 06:13:19 +0100

    Update ENTRYFORM.vb

--- myproject/ENTRYFORM.vb
+++ myproject/ENTRYFORM.vb
@@ -6,6 +6,5 @@ Public Class ENTRYFORM
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
         Me.Hide()
         BIOMETRICS_BACKGROUND_FORM.Show()
-
     End Sub
 End Class
