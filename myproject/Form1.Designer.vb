ad8f7be206cdfbde5c86f9ac0df53a285c968fe4
Olaoye Mich <41102062+michson@users.noreply.github.com>
2023-03-04 20:17:57 +0100

    Update Form1.Designer.vb

--- myproject/Form1.Designer.vb
+++ myproject/Form1.Designer.vb
@@ -2,6 +2,7 @@
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
+
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
